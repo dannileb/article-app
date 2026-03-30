1949544c42a44e682e250e638f3cacef1c91c4ab
Danila Lebedev <l1endqwsa@gmail.com>
2026-03-30 18:46:11 +0300

    fixed tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,7 +3,10 @@ import path from 'path';
 
 const config: Config = {
     clearMocks: true,
-    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.[t|j]sx?$': 'babel-jest',
+    },
+    testEnvironment: 'jest-environment-jsdom',
     coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
     moduleDirectories: ['node_modules', 'src'],
     moduleFileExtensions: [
@@ -24,13 +27,14 @@ const config: Config = {
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             path.resolve(__dirname, 'jestEmptyComponent.tsx'),
-        '#/(.*)': '<rootDir>/src/$1',
+        '#/(.*)$': '<rootDir>/src/$1',
         '\\.(s?css|less)$': 'identity-obj-proxy',
     },
     globals: {
         __IS_DEV__: true,
         __API__: '',
         __PROJECT__: 'jest',
+        __BUNDLER__: 'webpack',
     },
     reporters: [
         'default',
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -39,6 +39,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
             __IS_DEV__: true,
             __API__: JSON.stringify(''),
             __PROJECT__: JSON.stringify('storybook'),
+            __BUNDLER__: JSON.stringify('webpack'),
         }),
     );
 
